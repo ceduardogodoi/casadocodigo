057fbc737fdc237688ebeabafd1027ef87277984
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-09-29 00:26:21 -0300

    Templates dinâmicos

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -14,7 +14,12 @@ module.exports = app => {
 
   app.get('/livros', function (req, resp) {
     resp.marko(
-      require('../views/livros/lista/lista.marko')
+      require('../views/livros/lista/lista.marko'), {
+      livros: [
+        { id: 1, titulo: 'Fundamentos de Node' },
+        { id: 2, titulo: 'Node avançado' }
+      ]
+    }
     );
   });
 };
